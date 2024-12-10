59f52411c5232d43915242dee6231f70a172ac75
Mizusaka <mizuho.sakaguchi8@gmail.com>
2024-12-10 12:06:10 +0900

    🐛 export defaltへの変更

--- src/pages/components/Footer.js
+++ src/pages/components/Footer.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export function Footer() {
+export default function Footer() {
   return (
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
