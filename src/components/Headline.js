0113d9be9ad0f70c6f6aa4624954be2bea62335e
Mizusaka <mizuho.sakaguchi8@gmail.com>
2024-12-10 17:17:51 +0900

    ♻️ ヘッドライン部分をコンポーネント化

---
+++ src/components/Headline.js
@@ -0,0 +1,40 @@
+// import React from "react";
+import Image from "next/image";
+import localFont from "next/font/local";
+
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
+
+export default function Hedline(props) {
+    // console.log(props.title);
+  return (
+    <>
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <li className="mb-2">
+            {props.title} by editing{" "}
+            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+              src/pages/{props.page}.js
+            </code>
+            .
+          </li>
+          <li>Save and see your changes instantly.</li>
+        </ol>
+    </>
+  );
+}
