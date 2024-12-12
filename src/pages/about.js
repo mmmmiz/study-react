e160d0edb064eb3cd803109d48f6793caef8a168
Mizusaka <mizuho.sakaguchi8@gmail.com>
2024-12-12 10:11:22 +0900

    named exportのimportの波括弧の付与

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import localFont from "next/font/local";
 import { Footer} from "../components/Footer";
-import Links from "../components/Links";
-import Hedline from "../components/Headline";
+import {Links} from "../components/Links";
+import {Hedline} from "../components/Headline";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -21,7 +21,7 @@ export default function Home() {
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Hedline title = "About Page" page="about"/>
+        <Hedline title = "About Page" page={111}/>
 <Links />
       </main>
 
