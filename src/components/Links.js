c22675d7b645c9fcdab5476525964673a627bab6
Mizusaka <mizuho.sakaguchi8@gmail.com>
2024-12-26 14:44:54 +0900

    ♻️ Linksのデータと処理部分の切り分け

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,11 +1,31 @@
 import Image from "next/image";
 
+const ITEMS = [
+  {
+    href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app",
+    description: "Deploy now \u2192",
+  },
+  {
+    href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app",
+    description: "Read our docs →",
+  },
+  {
+    href: "https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app",
+    description: "Deploy now2",
+  },
+  {
+    href: "https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app",
+    description: "Read our docs2",
+  },
+];
 export function Links() {
-  return (   
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
+  return (
+    <div className="flex gap-4 items-center flex-col sm:flex-row">
+      {ITEMS.map((item) => (
+              <a
+              key={item.description}
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+            href={item.href}
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -16,16 +36,9 @@ export function Links() {
               width={20}
               height={20}
             />
-            Deploy now
+            <p>{item.description}</p>
           </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+        ))}
+    </div>
   );
 }
