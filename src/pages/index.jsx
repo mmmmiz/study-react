7509a7e155b6b5e8d41ac0995ba6602cc1791541
Mizusaka <mizuho.sakaguchi8@gmail.com>
2024-12-27 19:32:46 +0900

    ⚡️ useCallback,useEffectの解説

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -22,10 +22,17 @@ const geistMono = localFont({
 export default function Home() {
   const [count,setCount] = useState(0);
 
-const handleClick = (e) =>{
-  setCount((count)=> count + 1);
-  setCount((count)=> count + 1);
-};
+// const handleClick = (e) =>{
+//   setCount((count)=> count + 1);
+//   setCount((count)=> count + 1);
+// };
+
+const handleClick = useCallback(()=>{
+  console.log(count);
+  if(count < 10){
+    setCount((count)=> count + 1);
+  }
+},[count]);
 
   useEffect(() => {
     console.log("マウント時");
