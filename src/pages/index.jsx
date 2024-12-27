1a66e519d78a6a7d8bbee105b07dba62254c9bbf
Mizusaka <mizuho.sakaguchi8@gmail.com>
2024-12-27 15:23:59 +0900

    ⚗️ いづれ削除するクリックイベント追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 import localFont from "next/font/local";
-import { Footer} from "../components/Footer";
-import{Main} from "../components/Main";
+import { Footer } from "../components/Footer";
+import { Main } from "../components/Main";
 import { Header } from "../components/Header";
-
+import { useCallback } from "react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -17,13 +17,23 @@ const geistMono = localFont({
 });
 
 export default function Home() {
+  const handleClick= useCallback((e)=> {
+    console.log(e.target)
+    e.preventDefault();
+  },[]);
+  
   return (
-    
     <div
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
-      <Header/>
-<Main page="index"/>
+      <Header />
+     
+      <Main page="index" />
+      <a href="/about"
+        onClick={handleClick}
+      >
+        ボタン
+      </a>
       <Footer />
     </div>
   );
