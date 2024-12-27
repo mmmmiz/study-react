27d0fcaec4683ccab9575acc8bb1c431ccca9577
Mizusaka <mizuho.sakaguchi8@gmail.com>
2024-12-27 15:31:27 +0900

    🐛 アンカー削除

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,6 +4,7 @@ import { Footer } from "../components/Footer";
 import { Main } from "../components/Main";
 import { Header } from "../components/Header";
 import { useCallback } from "react";
+import Link from "next/link";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -21,7 +22,7 @@ export default function Home() {
     console.log(e.target)
     e.preventDefault();
   },[]);
-  
+
   return (
     <div
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
@@ -29,11 +30,10 @@ export default function Home() {
       <Header />
      
       <Main page="index" />
-      <a href="/about"
-        onClick={handleClick}
-      >
+      <Link href="/about"
+        onClick={handleClick}>
         ボタン
-      </a>
+      </Link>
       <Footer />
     </div>
   );
