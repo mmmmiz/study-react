6074135932314b2f3f0fca0391ee9b6d8d6a1844
Mizusaka <mizuho.sakaguchi8@gmail.com>
2024-12-27 16:39:46 +0900

    ✨ カウンター設置してuseStateでカウントアップ

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,6 +6,7 @@ import { Header } from "../components/Header";
 import { useCallback } from "react";
 import Link from "next/link";
 import { useEffect } from "react";
+import { useState } from "react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -19,6 +20,13 @@ const geistMono = localFont({
 });
 
 export default function Home() {
+  const [count,setCount] = useState(0);
+
+const handleClick = (e) =>{
+  setCount((count)=> count + 1);
+  setCount((count)=> count + 1);
+};
+
   useEffect(() => {
     console.log("マウント時");
     document.body.style.backgroundColor = "lightblue";
@@ -28,10 +36,10 @@ export default function Home() {
     }
       },[]);
 
-  const handleClick= useCallback((e)=> {
-    console.log(e.target)
-    e.preventDefault();
-  },[]);
+  // const handleClick= useCallback((e)=> {
+  //   console.log(e.target)
+  //   e.preventDefault();
+  // },[]);
 
   return (
     <div
@@ -40,10 +48,12 @@ export default function Home() {
       <Header />
      
       <Main page="index" />
-      <Link href="/about"
+      <h1>{count}</h1>
+      <button onClick={handleClick}>ボタン</button>
+      {/* <Link href="/about"
         onClick={handleClick}>
         ボタン
-      </Link>
+      </Link> */}
       <Footer />
     </div>
   );
